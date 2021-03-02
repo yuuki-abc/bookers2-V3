7dfb817bf4f1c563b56a6938a3fcbcb05c5b34e5
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-02 11:53:28 +0000

    [質問] params[:user_id]で値を受け取れなかったのは何故？リレーションシップコントローラーの中

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,16 @@
 class RelationshipsController < ApplicationController
 
   def create
-    relationships = Relationships.new(follower_user: current_user.id)
-    relationships.followed_user = User.find(params[:id])
+    relationships = Relationship.new(follower_id: current_user.id)
+    binding.pry
+    a = '123'
+    relationships.followed_id = User.find(params[:user_id])
     relationships.save
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    relationships = Relationships.find_by(followed_user: params[:id], follower_user: current_user)
+    relationships = Relationship.find_by(followed_user: params[:id], follower_user: current_user)
     relationships.destroy
     redirect_back(fallback_location: root_path)
   end
