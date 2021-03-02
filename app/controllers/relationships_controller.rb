d65bd3898a1f9ba03f1543a030529441bdb4aeed
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-02 11:17:09 +0000

    アクションの内容が完成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,14 @@
 class RelationshipsController < ApplicationController
 
   def create
+    relationships = Relationships.new(follower_user: current_user.id)
+    relationships.followed_user = User.find(params[:id])
+    relationships.save
   end
 
   def destroy
+    relationships = Relationships.find_by(followed_user: params[:id], follower_user: current_user)
+    relationships.destroy
   end
 
 end
